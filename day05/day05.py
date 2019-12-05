d20981089399db65abee3f3e8a4fb957116325e2
Jon Fincher <jfincher42@gmail.com>
2019-12-05 09:08:20 -0600

    Day 5 part 2 done!

--- day05/day05.py
+++ day05/day05.py
@@ -5,6 +5,7 @@ def opcode_00():
     print("ERROR")
 
 
+# Add two numbers
 def opcode_01(memory: list, ip: int, modes: str):
 
     # Get the parameters
@@ -25,6 +26,7 @@ def opcode_01(memory: list, ip: int, modes: str):
     return ip + 4
 
 
+# Multiple two numbers
 def opcode_02(memory: list, ip: int, modes: str):
 
     # Get the parameters
@@ -44,6 +46,7 @@ def opcode_02(memory: list, ip: int, modes: str):
     return ip + 4
 
 
+# Get user input
 def opcode_03(memory: list, ip: int, modes: str):
 
     # Get user input
@@ -57,6 +60,7 @@ def opcode_03(memory: list, ip: int, modes: str):
     return ip + 2
 
 
+# Print output
 def opcode_04(memory: list, ip: int, modes: str):
 
     # Get parameteres
@@ -68,8 +72,104 @@ def opcode_04(memory: list, ip: int, modes: str):
     return ip + 2
 
 
+# Jump if true
+def opcode_05(memory: list, ip: int, modes: str):
+
+    # Get the parameters
+    if modes[2] == "0":
+        param1 = memory[memory[ip + 1]]
+    else:
+        param1 = memory[ip + 1]
+
+    if modes[1] == "0":
+        param2 = memory[memory[ip + 2]]
+    else:
+        param2 = memory[ip + 2]
+
+    if param1 != 0:
+        ip = param2
+    else:
+        ip += 3
+
+    return ip
+
+
+# Jump if false
+def opcode_06(memory: list, ip: int, modes: str):
+
+    # Get the parameters
+    if modes[2] == "0":
+        param1 = memory[memory[ip + 1]]
+    else:
+        param1 = memory[ip + 1]
+
+    if modes[1] == "0":
+        param2 = memory[memory[ip + 2]]
+    else:
+        param2 = memory[ip + 2]
+
+    if param1 == 0:
+        ip = param2
+    else:
+        ip += 3
+
+    return ip
+
+
+# Less than
+def opcode_07(memory: list, ip: int, modes: str):
+
+    # Get the parameters
+    if modes[2] == "0":
+        param1 = memory[memory[ip + 1]]
+    else:
+        param1 = memory[ip + 1]
+
+    if modes[1] == "0":
+        param2 = memory[memory[ip + 2]]
+    else:
+        param2 = memory[ip + 2]
+
+    if modes[0] == "0":
+        memory[memory[ip + 3]] = int(param1 < param2)
+    else:
+        memory[ip + 3] = int(param1 < param2)
+
+    return ip+4
+
+# Equals
+def opcode_08(memory: list, ip: int, modes: str):
+
+    # Get the parameters
+    if modes[2] == "0":
+        param1 = memory[memory[ip + 1]]
+    else:
+        param1 = memory[ip + 1]
+
+    if modes[1] == "0":
+        param2 = memory[memory[ip + 2]]
+    else:
+        param2 = memory[ip + 2]
+
+    if modes[0] == "0":
+        memory[memory[ip + 3]] = int(param1 == param2)
+    else:
+        memory[ip + 3] = int(param1 == param2)
+
+    return ip+4
+
 # Table of opcode functions
-opcode_functions = [opcode_00, opcode_01, opcode_02, opcode_03, opcode_04]
+opcode_functions = [
+    opcode_00,
+    opcode_01,
+    opcode_02,
+    opcode_03,
+    opcode_04,
+    opcode_05,
+    opcode_06,
+    opcode_07,
+    opcode_08,
+]
 
 
 # The IntCode computer
