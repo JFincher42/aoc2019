bcb3859e76ab6c7edc875109a37bc2f5f5c28f20
Jon Fincher <jfincher42@gmail.com>
2019-12-06 09:04:13 -0600

    Day 6 Part 2 done, wow is this ugly...

--- day06/day06.py
+++ day06/day06.py
@@ -7,6 +7,23 @@ def count_bodies(planets, orbits, level):
     return count
 
 
+def find_route(orbits, start, end):
+    route = []
+    if start not in orbits:
+        return route
+
+    elif end in orbits[start]:
+        return [start]
+
+    else:
+        for body in orbits[start]:
+            temp = find_route(orbits, body, end)
+            if len(temp) > 0:
+                route.append(start)
+                route.extend(temp)
+
+    return route
+
 if __name__ == "__main__":
 
     # Open the file, process into dict
@@ -34,3 +51,45 @@ if __name__ == "__main__":
         count += count_bodies(orbits[body], orbits, level)
 
     print(f"# of Orbits = {count}")
+
+    # Part 2: First, figure out where each of us is
+
+    for k,v in orbits.items():
+        if "YOU" in v:
+            my_loc = k
+        if "SAN" in v:
+            san_loc = k
+
+    # Find my path to COM
+    my_path = find_route(orbits, 'COM', my_loc)
+    san_path = find_route(orbits, 'COM', san_loc)
+    my_path.append(my_loc)
+    san_path.append(san_loc)
+
+    # Find the first common point
+    # Reverse them to make it easier
+    my_path.reverse()
+    san_path.reverse()
+
+    my_path_count = 0
+    # First, find the point in my location list
+    for body in my_path:
+        if body in san_path:
+            common = body
+            break
+        else:
+            my_path_count += 1
+
+
+    # Now count the items between them
+    san_path_count = 0
+    for body in san_path:
+        if body == common:
+            break
+        else:
+            san_path_count+=1
+
+
+    print(f"   My Location and Path:{my_loc} and {my_path}")
+    print(f"Santa Location and Path:{san_loc} and {san_path}")
+    print(f"Orbital transfers required = {my_path_count + san_path_count}")
