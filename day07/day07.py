564252fa0638bd34fdffc5d3056ad4e794296b36
Jon Fincher <jfincher42@gmail.com>
2019-12-08 18:35:00 -0600

    Day 7 part 2 done!

--- day07/day07.py
+++ day07/day07.py
@@ -1,5 +1,5 @@
 from typing import Tuple
-from intcode import intcode
+from intcode import IntCode
 import sys
 import io
 from contextlib import redirect_stdout
@@ -12,23 +12,49 @@ def run_test(phases: Tuple, init_strength: int):
 
     # Loop through phases given
     for phase in phases:
-        # Redirect output to a new buffere
-        out_bytes = io.BytesIO()
-        out_buf = io.TextIOWrapper(
-            out_bytes,
-            sys.stdout.encoding,
-            line_buffering=True,
-            write_through=True,
-        )
+
+        intcode = IntCode(memory, 0)
+        intcode.write_input(phase)
+        intcode.write_input(strength)
+        intcode.run()
+        strength = int(intcode.read_output())
+
+    # Return the output strength
+    return int(strength)
+
+
+def all_halted(amps):
+    return (
+        amps[0].is_halted()
+        and amps[1].is_halted()
+        and amps[2].is_halted()
+        and amps[3].is_halted()
+        and amps[4].is_halted()
+    )
+
+
+def run_test_loop(amplifiers: list, phases: Tuple, init_strength: int):
+
+    strength = str(init_strength)
+    current_phase = 0
+
+    # Current amp
+    amp = 0
+
+    # keep going until all amplifiers are halted
+    while not all_halted(amplifiers):
+        # Get current phase, if any
+        if current_phase < len(phases):
+            amplifiers[amp].write_input(phases[current_phase])
+            current_phase += 1
 
         # Run the program with the proper input
-        with redirect_stdout(out_buf):
-            sys.stdin = io.StringIO(phase + "\n" + strength + "\n")
-            intcode(memory)
+        amplifiers[amp].write_input(strength)
+        amplifiers[amp].run()
+        strength = amplifiers[amp].read_output()
 
-            # Get the output strength
-            out_buf.seek(0)
-            strength = out_buf.readline().strip()
+        # Next amp
+        amp = (amp + 1) % 5
 
     # Return the output strength
     return int(strength)
@@ -51,4 +77,23 @@ if __name__ == "__main__":
 
     print(f"Part 1 Final: {max_strength}")
 
-    #
+    # Part 2: New problem, same file, but need it fresh
+    with open("day07/input.txt") as f:
+        memory = [int(x) for x in f.readline().split(",")]
+
+    # Set our current max strength
+    max_strength = 0
+
+    # Loop through all our possible permutations
+    for phase in itertools.permutations("56789", 5):
+
+        # Init five different amplifiers
+        amplifiers = []
+        for _ in range(5):
+            amplifiers.append(IntCode(memory, 0))
+
+        strength = run_test_loop(amplifiers, phase, 0)
+        if strength > max_strength:
+            max_strength = strength
+
+    print(f"Part 2 Final: {max_strength}")
--- day07/intcode.py
+++ day07/intcode.py
@@ -1,196 +1,247 @@
 ### IntCode computer
 from typing import List
-
-# Something has gone horribly wrong, just end it all
-def opcode_00(memory: list, ip: int, modes: str):
-    print("ERROR")
-    return len(memory)
+from collections import deque
 
 
-# Add two numbers
-def opcode_01(memory: list, ip: int, modes: str):
+class IntCode:
+    def __init__(self, memory: List, ip: int = 0):
 
-    # Get the parameters
-    if modes[2] == "0":
-        param1 = memory[memory[ip + 1]]
-    else:
-        param1 = memory[ip + 1]
+        self._memory = memory.copy()
+        self._ip = ip
+        self._mem_len = len(self._memory)
 
-    if modes[1] == "0":
-        param2 = memory[memory[ip + 2]]
-    else:
-        param2 = memory[ip + 2]
+        # Internal state
+        self._paused = False
+        self._halted = False
 
-    # Third parameter is always 0
-    memory[memory[ip + 3]] = param1 + param2
+        # Input and output queues
+        self._input = deque([])
+        self._output = deque([])
+        self._last_output = ""
 
-    # Where is the next IP?
-    return ip + 4
+        # Opcode function table
+        self._opcode_functions = [
+            self._opcode_00,
+            self._opcode_01,
+            self._opcode_02,
+            self._opcode_03,
+            self._opcode_04,
+            self._opcode_05,
+            self._opcode_06,
+            self._opcode_07,
+            self._opcode_08,
+        ]
 
+    def is_halted(self):
+        return self._halted
 
-# Multiple two numbers
-def opcode_02(memory: list, ip: int, modes: str):
+    # Something has gone horribly wrong, just end it all
+    def _opcode_00(self, modes: str):
+        print("ERROR")
+        self._halted = True
+        return self._mem_len
 
-    # Get the parameters
-    if modes[2] == "0":
-        param1 = memory[memory[ip + 1]]
-    else:
-        param1 = memory[ip + 1]
+    # Add two numbers
+    def _opcode_01(self, modes: str):
 
-    if modes[1] == "0":
-        param2 = memory[memory[ip + 2]]
-    else:
-        param2 = memory[ip + 2]
+        # Get the parameters
+        if modes[2] == "0":
+            param1 = self._memory[self._memory[self._ip + 1]]
+        else:
+            param1 = self._memory[self._ip + 1]
 
-    memory[memory[ip + 3]] = param1 * param2
+        if modes[1] == "0":
+            param2 = self._memory[self._memory[self._ip + 2]]
+        else:
+            param2 = self._memory[self._ip + 2]
 
-    # Where is the next IP?
-    return ip + 4
+        # Third parameter is always 0
+        self._memory[self._memory[self._ip + 3]] = param1 + param2
 
+        # Where is the next self._IP?
+        return self._ip + 4
 
-# Get user input
-def opcode_03(memory: list, ip: int, modes: str):
+    # Multiply two numbers
+    def _opcode_02(self, modes: str):
+
+        # Get the parameters
+        if modes[2] == "0":
+            param1 = self._memory[self._memory[self._ip + 1]]
+        else:
+            param1 = self._memory[self._ip + 1]
+
+        if modes[1] == "0":
+            param2 = self._memory[self._memory[self._ip + 2]]
+        else:
+            param2 = self._memory[self._ip + 2]
+
+        self._memory[self._memory[self._ip + 3]] = param1 * param2
+
+        # Where is the next self._IP?
+        return self._ip + 4
 
     # Get user input
-    user_input = int(input())
+    def _opcode_03(self, modes: str):
 
-    if modes[2] == "0":
-        memory[memory[ip + 1]] = user_input
-    else:
-        memory[ip + 1] = user_input
+        # Get user input
+        user_input = int(self._read_input())
 
-    return ip + 2
+        if modes[2] == "0":
+            self._memory[self._memory[self._ip + 1]] = user_input
+        else:
+            self._memory[self._ip + 1] = user_input
 
+        return self._ip + 2
 
-# Print output
-def opcode_04(memory: list, ip: int, modes: str):
+    # Print output
+    def _opcode_04(self, modes: str):
 
-    # Get parameteres
-    if modes[2] == "0":
-        print(f"{memory[memory[ip + 1]]}")
-    else:
-        print(f"{memory[ip + 1]}")
+        # Get parameters
+        if modes[2] == "0":
+            self._write_output(f"{self._memory[self._memory[self._ip + 1]]}")
+        else:
+            self._write_output(f"{self._memory[self._ip + 1]}")
 
-    return ip + 2
+        self._paused = True
+        return self._ip + 2
 
+    # Jump if true
+    def _opcode_05(self, modes: str):
 
-# Jump if true
-def opcode_05(memory: list, ip: int, modes: str):
+        # Get the parameters
+        if modes[2] == "0":
+            param1 = self._memory[self._memory[self._ip + 1]]
+        else:
+            param1 = self._memory[self._ip + 1]
 
-    # Get the parameters
-    if modes[2] == "0":
-        param1 = memory[memory[ip + 1]]
-    else:
-        param1 = memory[ip + 1]
+        if modes[1] == "0":
+            param2 = self._memory[self._memory[self._ip + 2]]
+        else:
+            param2 = self._memory[self._ip + 2]
 
-    if modes[1] == "0":
-        param2 = memory[memory[ip + 2]]
-    else:
-        param2 = memory[ip + 2]
+        if param1 != 0:
+            self._ip = param2
+        else:
+            self._ip += 3
 
-    if param1 != 0:
-        ip = param2
-    else:
-        ip += 3
+        return self._ip
 
-    return ip
+    # Jump if false
+    def _opcode_06(self, modes: str):
 
+        # Get the parameters
+        if modes[2] == "0":
+            param1 = self._memory[self._memory[self._ip + 1]]
+        else:
+            param1 = self._memory[self._ip + 1]
 
-# Jump if false
-def opcode_06(memory: list, ip: int, modes: str):
+        if modes[1] == "0":
+            param2 = self._memory[self._memory[self._ip + 2]]
+        else:
+            param2 = self._memory[self._ip + 2]
 
-    # Get the parameters
-    if modes[2] == "0":
-        param1 = memory[memory[ip + 1]]
-    else:
-        param1 = memory[ip + 1]
+        if param1 == 0:
+            self._ip = param2
+        else:
+            self._ip += 3
 
-    if modes[1] == "0":
-        param2 = memory[memory[ip + 2]]
-    else:
-        param2 = memory[ip + 2]
+        return self._ip
 
-    if param1 == 0:
-        ip = param2
-    else:
-        ip += 3
+    # Less than
+    def _opcode_07(self, modes: str):
 
-    return ip
+        # Get the parameters
+        if modes[2] == "0":
+            param1 = self._memory[self._memory[self._ip + 1]]
+        else:
+            param1 = self._memory[self._ip + 1]
 
+        if modes[1] == "0":
+            param2 = self._memory[self._memory[self._ip + 2]]
+        else:
+            param2 = self._memory[self._ip + 2]
 
-# Less than
-def opcode_07(memory: list, ip: int, modes: str):
+        if modes[0] == "0":
+            self._memory[self._memory[self._ip + 3]] = int(param1 < param2)
+        else:
+            self._memory[self._ip + 3] = int(param1 < param2)
 
-    # Get the parameters
-    if modes[2] == "0":
-        param1 = memory[memory[ip + 1]]
-    else:
-        param1 = memory[ip + 1]
+        return self._ip + 4
 
-    if modes[1] == "0":
-        param2 = memory[memory[ip + 2]]
-    else:
-        param2 = memory[ip + 2]
+    # Equals
+    def _opcode_08(self, modes: str):
 
-    if modes[0] == "0":
-        memory[memory[ip + 3]] = int(param1 < param2)
-    else:
-        memory[ip + 3] = int(param1 < param2)
+        # Get the parameters
+        if modes[2] == "0":
+            param1 = self._memory[self._memory[self._ip + 1]]
+        else:
+            param1 = self._memory[self._ip + 1]
 
-    return ip + 4
+        if modes[1] == "0":
+            param2 = self._memory[self._memory[self._ip + 2]]
+        else:
+            param2 = self._memory[self._ip + 2]
 
+        if modes[0] == "0":
+            self._memory[self._memory[self._ip + 3]] = int(param1 == param2)
+        else:
+            self._memory[self._ip + 3] = int(param1 == param2)
 
-# Equals
-def opcode_08(memory: list, ip: int, modes: str):
+        return self._ip + 4
 
-    # Get the parameters
-    if modes[2] == "0":
-        param1 = memory[memory[ip + 1]]
-    else:
-        param1 = memory[ip + 1]
+    # I/O functions
 
-    if modes[1] == "0":
-        param2 = memory[memory[ip + 2]]
-    else:
-        param2 = memory[ip + 2]
+    def write_input(self, item: str):
+        self._input.append(item)
 
-    if modes[0] == "0":
-        memory[memory[ip + 3]] = int(param1 == param2)
-    else:
-        memory[ip + 3] = int(param1 == param2)
+    def _read_input(self):
+        return self._input.popleft()
 
-    return ip + 4
+    def _write_output(self, item: str):
+        self._output.append(item)
 
+    def read_output(self):
+        if len(self._output):
+            self._last_output = self._output.popleft()
+        return self._last_output
 
-# Table of opcode functions
-opcode_functions = [
-    opcode_00,
-    opcode_01,
-    opcode_02,
-    opcode_03,
-    opcode_04,
-    opcode_05,
-    opcode_06,
-    opcode_07,
-    opcode_08,
-]
+    # The IntCode computer
+    def run(self):
 
+        # First check if we're halted
 
-# The IntCode computer
-def intcode(memory: List):
+        if self._halted:
+            return -1
 
-    # Setup the computer
-    ip = 0
+        # Unpause
+        self._paused = False
 
-    # Run the program
-    while ip < len(memory) and memory[ip] != 99:
-        # Encode the opcode
-        opcode = f"{memory[ip]:05}"
+        # Check if we should be halted
+        self._halted = (
+            self._ip == -1
+            or self._ip >= self._mem_len
+            or self._memory[self._ip] == 99
+        )
 
-        # Get the instruction
-        instruction = int(opcode[3:])
+        # Run the program
+        while (
+            self._ip < self._mem_len and not self._paused and not self._halted
+        ):
+            # Encode the opcode
+            opcode = f"{self._memory[self._ip]:05}"
 
-        # Process the opcode
-        ip = opcode_functions[instruction](memory, ip, opcode[0:3])
+            # Get the instruction
+            instruction = int(opcode[3:])
 
-    # print("HALT")
+            # Process the opcode
+            if instruction == 99:
+                self._halted = True
+            else:
+                self._ip = self._opcode_functions[instruction](
+                    opcode[0:3]
+                )
+
+        if self._halted:
+            return -1
+        else:
+            return self._ip
