247f417d485ed54c1c347d4a36e61db0872ea0c0
Jon Fincher <jfincher42@gmail.com>
2019-12-09 08:56:03 -0600

    Day 9 part 1

---
+++ day09/day09.py
@@ -0,0 +1,16 @@
+from intcode import IntCode
+
+if __name__ == "__main__":
+
+    # Read input
+    memory = []
+    with open("day09/input.txt") as f:
+        for line in f.readlines():
+            memory.extend([int(x) for x in line.split(",")])
+
+    # Create new IntCode instance
+    intcode = IntCode(memory, 0)
+    intcode.write_input("1")
+    while not intcode.is_halted():
+        intcode.run()
+        print(f"{intcode.read_output()}")
