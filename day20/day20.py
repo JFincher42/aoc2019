a7e4d3cb6cbb22ca097fc50c869561fd0d1ee767
Jon Fincher <jfincher42@gmail.com>
2019-12-21 09:40:22 -0600

    Day 20 part 2 done

--- day20/day20.py
+++ day20/day20.py
@@ -1,4 +1,5 @@
 PORTAL_LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+LEVEL_LIMIT = 25
 
 
 def find_portals(maze):
@@ -35,6 +36,20 @@ def find_portals(maze):
                         portals[portal_name].append(location)
                     else:
                         portals[portal_name] = [location]
+
+    # Order the portals, outer in portals[0], inner in portals[1]
+    for portal_name in portals.keys():
+        if portal_name == "AA" or portal_name == "ZZ":
+            pass
+        elif (
+            portals[portal_name][1][0] == 2
+            or portals[portal_name][1][1] == 2
+            or portals[portal_name][1][0] == len(maze[0]) - 4
+            or portals[portal_name][1][1] == len(maze) - 3
+        ):
+            temp = portals[portal_name][1]
+            portals[portal_name][1] = portals[portal_name][0]
+            portals[portal_name][0] = temp
 
 
 def find_shortest_path(start, end, steps, maze):
@@ -82,8 +97,75 @@ def find_shortest_path(start, end, steps, maze):
                 seen.add(neighbor)
 
 
+def find_shortest_path2(start, end, maze):
+
+    # Use BFS to find the shortest path to the exit
+
+    next_pos = [(start, 0, 0)]
+    seen = {}
+    seen[0] = set(start)
+
+    while next_pos:
+        current = next_pos.pop()
+        current_pos = current[0]
+        steps = current[1]
+        current_level = current[2]
+
+        # Limit the levels
+        if current_level > LEVEL_LIMIT:
+            continue
+
+        # Create our tracker if it's not there
+        if current_level + 1 not in seen.keys():
+            seen[current_level + 1] = set()
+
+        # Do we have this maze?
+        if current_pos == end and current_level == 0:
+            return steps
+
+        # Are we at a portal?
+        for portal in portals.keys():
+            # Ignore the start and end
+            if portal == "AA" or portal == "ZZ":
+                pass
+
+            # Ignore outer levels on level 0
+            elif (
+                current_level > 0
+                and current_pos == portals[portal][0]
+                and portals[portal][1] not in seen[current_level - 1]
+            ):
+                next_pos.append(
+                    (portals[portal][1], steps + 1, current_level - 1)
+                )
+                seen[current_level - 1].add(portals[portal][1])
+
+            # Inner portal check
+            elif (
+                current_pos == portals[portal][1]
+                and portals[portal][0] not in seen[current_level + 1]
+            ):
+                next_pos.append(
+                    (portals[portal][0], steps + 1, current_level + 1)
+                )
+                seen[current_level + 1].add(portals[portal][0])
+
+        # Check everything else around us
+        for dir in [(0, -1), (-1, 0), (0, 1), (1, 0)]:
+            neighbor = (
+                current_pos[0] + dir[0],
+                current_pos[1] + dir[1],
+            )
+            if (
+                maze[neighbor[1]][neighbor[0]] == "."
+                and neighbor not in seen[current_level]
+            ):
+                next_pos.append((neighbor, steps + 1, current_level))
+                seen[current_level].add(neighbor)
+
+
 if __name__ == "__main__":
-    global portals, paths, visited
+    global portals, paths
 
     maze = []
     portals = {}
@@ -98,6 +180,8 @@ if __name__ == "__main__":
     # Locate all the portals
     find_portals(maze)
 
+    print(f"Maze dimensions: ({len(maze)}, {len(maze[0])})")
+
     print(f"Portals: {portals}")
 
     # Find the shortest path
@@ -105,4 +189,12 @@ if __name__ == "__main__":
     paths[portals["AA"][0]] = 0
     visited = set()
     shortest = find_shortest_path(portals["AA"][0], portals["ZZ"][0], 0, maze)
+    print(f"Shortest path to ZZ = {shortest}")
+
+    # Part 2
+    # Ignore all outer portals on level 0, ignore AA and ZZ on all others
+    current_level = 0
+
+    visited = set()
+    shortest = find_shortest_path2(portals["AA"][0], portals["ZZ"][0], maze)
     print(f"Shortest path to ZZ = {shortest}")
